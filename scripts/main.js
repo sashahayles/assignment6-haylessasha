9d0b6ea0286795611f004eb439970baa802bab1e
Sasha Hayles <sashahayles@ufl.edu>
2019-04-27 23:58:21 -0400

    created js loop

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+var members = [
+  'Dave Grohl',
+  'Nate Mendel',
+  'Pat Smear',
+  'Taylor Hawkins',
+  'Rami Jaffee',
+  'Chris Shiflett'
+];
+
+var tour= {
+  chicago: 'July 29th & 30th, 2018',
+  seattle: 'September 1st,',
+  edmonton:' 2018 September 4th, 2018'
+};
+
+var x = 0;
+
+while (x < 5) {
+  console.log ('Times running through the loop x:' + x);
+  i++;
+}
